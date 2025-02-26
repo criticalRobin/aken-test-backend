263623cb10b8c76025a7fec914f1d324865d1de5
criticalRobin <matiastite1@gmail.com>
2025-02-26 17:33:51 -0500

    Creado modelo scrap

--- scraper/models.py
+++ scraper/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class News(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=100)
+    time = models.CharField(max_length=50)
+    summary = models.TextField()
+
+    def __str__(self):
+        return self.title
